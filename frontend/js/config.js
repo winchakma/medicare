fa29c43aeba54664d1f596e233e85a7706887aaa
win chakma <163110097+winchakma@users.noreply.github.com>
2026-06-11 13:59:32 +0600

    Update frontend to use actual Render deployed backend URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -3,6 +3,6 @@
  */
 (function() {
     // Pure production API URL - NO LOCALHOST 
-    window.MEDICARE_API_URL = 'https://medicare-api-backend.onrender.com';
+    window.MEDICARE_API_URL = 'https://medicare-04b4.onrender.com';
     console.log(`MediCare API Target: ${window.MEDICARE_API_URL}`);
 })();
